3252e82f26b41ba713423a4048fc1684d5f7c9b6
Erick Almeida <almeida.ericks29@gmail.com>
2019-07-07 22:26:30 +1000

    Adding POST, GET, PUT, DELETE requests for users

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using BetAPI.Services;
+using BetAPI.Entities;
 using System.Collections.Generic;
 using System;
 namespace BetAPI.Controllers
 {
+     [Produces("application/json", "application/xml")]
      [Route("api/[controller]")]
      public class UsersController : Controller
      {
@@ -30,5 +32,50 @@ namespace BetAPI.Controllers
                   return Ok(userFilter);
               }
           }
+
+          // POST api/users
+          [HttpPost]
+          public IActionResult PostUser([FromBody]User user)
+          {
+               if (user == null)
+               {
+                    return BadRequest();
+               }
+               _context.Add(user);
+               _context.SaveChanges();
+               return CreatedAtRoute(nameof(GetUsersById), new { id = user.Id }, user);
+          }
+
+          // PUT api/users/guid
+          [HttpPut("{id}")]
+          public IActionResult PutUser(int id, [FromBody]User user)
+          {
+               if (user == null)
+               {
+                    return BadRequest();
+               }
+               var putUser = _context.Users.Find(id);
+               if (putUser == null)
+               {
+                    return NotFound();
+               }
+               putUser.Name = user.Name;
+               putUser.Age = user.Age;
+               _context.Update(putUser);
+               _context.SaveChanges();
+               return new NoContentResult();
+          }
+
+          // DELETE api/users/guid
+          [HttpDelete("{id}")]
+          public void Delete(int id)
+          {
+               var putUser = _context.Users.Find(id);
+               if (putUser != null)
+               {
+                    _context.Users.Remove(putUser);
+                    _context.SaveChanges();
+               }
+          }         
      }
 }
